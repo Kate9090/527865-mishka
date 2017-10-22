ce1cbc2a76c5580e88195aa4f6cc06109a17b608
Екатерина Еремина <vam.pochta@yahoo.com>
2017-10-22 23:36:33 +0300

    fff

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,78 @@ var gulp = require("gulp");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
-var posthtml = require("gulp-posthtml");
-var include = require("posthtml-include");
 var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
 var minify = require("gulp-csso");
+var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
-var rename = require("gulp-rename");
-var server = require("browser-sync").create();
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 var run = require("run-sequence");
 var del = require("del");
+var uglify = require("gulp-uglify");
 
-gulp.task("clean", function (){
-  return del("build");
+gulp.task("style", function() {
+  gulp.src("less/style.less")
+    .pipe(plumber())
+    .pipe(less())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream())
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"));
+});
+
+
+gulp.task("serve", function() {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("less/**/*.less", ["style"]);
+  gulp.watch("*.html", ["html"]);
+});
+
+gulp.task("images", function() {
+  return gulp.src("img/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("webp", function() {
+  return gulp.src("img/**/*.{png,ipg}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("img"));
+});
+
+gulp.task("sprite", function() {
+  return gulp.src("img/icon-*.svg")
+    .pipe(svgstore({
+      inLineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function() {
+  return gulp.src("*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
 });
 
 gulp.task("copy", function() {
@@ -31,62 +89,18 @@ gulp.task("copy", function() {
     .pipe(gulp.dest("build"));
 });
 
-gulp.task("style", function() {
-  gulp.src("less/style.less")
-    .pipe(plumber())
-    .pipe(less())
-    .pipe(postcss([
-      autoprefixer()
-    ]))
-    .pipe(gulp.dest("build/css"))
-    .pipe(minify())
-    .pipe(rename("style.min.css"))
-    .pipe(gulp.dest("build/css"));
-
+gulp.task("clean", function() {
+  return del("build");
 });
 
-gulp.task("sprite", function() {
-  return gulp.src("img/*.svg")
-    .pipe(svgstore({
-      inlineSvg: true
-    }))
-    .pipe(rename("sprite.svg"))
-    .pipe(gulp.dest("build/img"));
+
+gulp.task("scripts", function() {
+  return gulp.src("js/*.js")
+    .pipe(uglify())
+    .pipe(rename("script.min.js"))
+    .pipe(gulp.dest("build/js"));
 });
 
-gulp.task("html",function() {
-  return gulp.src("*.html")
-    .pipe(posthtml([
-      include()
-    ]))
-    .pipe(gulp.dest("build"));
-});
-
-gulp.task("images", function() {
-  return gulp.src("img/**/*.{png,jpg,svg}")
-    .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.svgo()
-    ]))
-    .pipe(gulp.dest("img"));
-});
-
-gulp.task("webp", function() {
-  return gulp.src("img/**/*.{png,jpg}")
-    .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("img"));
-});
-
-gulp.task("serve", function() {
-  server.init({
-    server: "build/"
-  });
-
-  gulp.watch("less/**/*.less", ["style"]);
-  gulp.watch(".html", ["html"]);
-  gulp.watch("js/*.js").on("change", server.reload);
-});
 
 gulp.task("build", function(done) {
   run(
@@ -94,6 +108,7 @@ gulp.task("build", function(done) {
     "copy",
     "style",
     "sprite",
+    "scripts",
     "html",
     done
   );
